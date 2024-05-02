7957a7a1db75393e4937abfa060dcb2f83f6ca02
Hovixen <orevaerijo@gmail.com>
2024-05-02 11:49:07 +0000

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 from collections import deque
-
+"""
+Lockbox concept that determines if all boxes can be opened
+"""
 
 def canUnlockAll(boxes):
+    """
+    Algorithm function that accepts the box list
+    returns: True if all boxes can be opened else False
+    """
+
     n = len(boxes)
     visited = set()
     visited.add(0)
