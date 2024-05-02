0652ce0814ac4b89810125ae14bd4ad1fb378ce0
Hovixen <orevaerijo@gmail.com>
2024-05-02 11:54:07 +0000

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from collections import deque
 """
 Lockbox concept that determines if all boxes can be opened
 """
+from collections import deque
+
 
 
 def canUnlockAll(boxes):
