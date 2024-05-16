0d3b00e55a440752f1e8eb6165bf4d8c1f2474bd
Hovixen <orevaerijo@gmail.com>
2024-05-16 14:26:44 +0000

    read me

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 """Module which contains minoperations function"""
 
-
 def minOperations(n):
-    """ Calculates the fewest number of operations needed
-        to result in exactly n H characters in the file
-
-        Args:
-            n: repetitions of H
-
-        Returns:
-            number of operations (Copy & Paste) to reach n Hs
     """
+    Calculates the minimum number of operations to reach n characters.
+    """
+    if n <= 1:
+        return 0
+    
     operations = 0
-    summation = 1
-    carrier = 0
-
-    while summation < n:
-        if n % summation == 0:  # Copy when summation is a multiple of n
-            carrier = summation
-            summation *= 2
-            operations += 1
+    divisor = 2
+    
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
         else:
-            summation += carrier
-        operations += 1  # Always paste
-
+            divisor += 1
+    
     return operations
