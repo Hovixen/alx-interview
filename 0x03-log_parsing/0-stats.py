ec439c53c373bedc873023717b52ebc3377e584f
Hovixen <orevaerijo@gmail.com>
2024-05-17 01:55:04 +0000

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
-
+""" Log parsing """
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+
+def line_in(line):
+    """ function process each line
+    returns status code and the file size
+    """
+    parts = line.split()
+    s_code = int(parts[-2])
+    f_size = int(parts[-1])
+    return s_code, f_size
+
+
+def print_stats(total_fs, s_code_count):
+    """ Prints the statistics """
+    print("File size: {}".format(total_fs))
+    for code, count in sorted(s_code_count.items()):
+        print("{}: {}".format(code, count))
+
 
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+    """ Prints stats from stdin """
+    total_fs = 0
+    s_code_count = {}
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    try:
+        line_count = 0
+        for line in sys.stdin:
+            s_code, f_size = line_in(line.rstrip())
+            if s_code is not None and f_size is not None:
+                total_fs += f_size
+                s_code_count[s_code] = s_code_count.get(s_code, 0) + 1
 
-except Exception as err:
-    pass
+            line_count += 1
 
+            if line_count == 10:
+                print_stats(total_fs, s_code_count)
+                line_count = 0
+    except Exception:
+        pass
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    print_stats(total_fs, s_code_count)
