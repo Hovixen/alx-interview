4ca5032ee66f20d59aa16c7c633066821926aaf4
Hovixen <orevaerijo@gmail.com>
2024-05-26 05:47:05 +0000

    test

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,30 +5,35 @@ validUTF8
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding"""
-    count = 0
+    """Number of bytes in the current UTF-8 character"""
+    num_bytes = 0
 
-    for x in data:
+    # Masks to check the most significant bits of a byte
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
 
-        if 191 >= x >= 128:
+    # Iterate over each integer in the data list
+    for num in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
 
-            if not count:
-                return False
-
-            count -= 1
-        else:
-            if count:
-                return False
-
-            if x < 128:
+            # 1 byte character
+            if num_bytes == 0:
                 continue
-            elif x < 224:
-                count = 1
-            elif x < 240:
-                count = 2
-            elif x < 248:
-                count = 3
-            else:
+
+            # If num_bytes is more than 4 or 1, it's invalid
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            # Check that the next byte starts with '10'
+            if not (num & mask1 and not (num & mask2)):
                 return False
 
-    return count == 0
+        num_bytes -= 1
+
+    # If we finish and num_bytes is not 0, then it's invalid
+    return num_bytes == 0
