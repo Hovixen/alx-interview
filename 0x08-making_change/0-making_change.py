376a69111b01b5875fb61bf20d9b922df98046ed
Hovixen <orevaerijo@gmail.com>
2024-06-19 06:33:32 +0000

    testing its efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,6 +6,28 @@ number of coins needed to meet a given amount total
 
 
 def makeChange(coins, total):
+    """
+    Minium coins required
+    """
+    if total <= 0:
+        return 0
+    trace, count = 0, 0
+    coins.sort()
+    coins = coins[::-1]
+    while len(coins) > 0:
+        value = coins[0]
+        if trace + value > total:
+            coins.pop(0)
+            continue
+        trace += value
+        count += 1
+        if trace == total:
+            return count
+    return -1
+
+
+"""
+def makeChange(coins, total):
     if total <= 0:
         return 0
 
@@ -21,3 +43,4 @@ def makeChange(coins, total):
 
     # Check if it's possible to form the total
     return dp[total] if dp[total] != float('inf') else -1
+"""
