60a70e1fdc1bec191ecac0302a8ae085d45cfdb5
Hovixen <orevaerijo@gmail.com>
2024-06-19 06:53:33 +0000

    using the greedy algorithm approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,24 +6,29 @@ number of coins needed to meet a given amount total
 
 
 def makeChange(coins, total):
-    """
-    Minium coins required
-    """
     if total <= 0:
         return 0
-    trace, count = 0, 0
-    coins.sort()
-    coins = coins[::-1]
-    while len(coins) > 0:
-        value = coins[0]
-        if trace + value > total:
-            coins.pop(0)
-            continue
-        trace += value
-        count += 1
-        if trace == total:
-            return count
-    return -1
+
+    # Sort the coins in descending order
+    coins.sort(reverse=True)
+    
+    count = 0
+    remaining = total
+
+    for coin in coins:
+        if remaining <= 0:
+            break
+        # Use as many of the current coin as possible
+        if coin <= remaining:
+            num_coins = remaining // coin
+            remaining -= num_coins * coin
+            count += num_coins
+
+    # Check if we were able to make the exact total
+    if remaining == 0:
+        return count
+    else:
+        return -1
 
 
 """
