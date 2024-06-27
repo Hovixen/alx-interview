c075dc30fe636efcdd86f88aa12d9f2917bc815d
Hovixen <orevaerijo@gmail.com>
2024-06-27 14:29:54 +0000

    task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Island perimeter function """
+
+def island_perimeter(grid):
+    """ function returns the perimeter of an island """
+    rows = len(grid)
+    columns = len(grid[0])
+
+    perimeter = 0
+
+    for m in range(rows):
+        for n in range(columns):
+            # check if element is land [1]
+            if grid[m][n] == 1:
+                # check if up is water [0]
+                if grid[m-1][n] == 0 or m == 0:
+                    perimeter += 1
+                # check if down is water [0]
+                if grid[m+1][n] == 0 or m == rows - 1:
+                    perimeter += 1
+                # check if left is water [0]
+                if grid[m][n-1] == 0 or n == 0:
+                    perimeter += 1
+                # check if right is water [0]
+                if grid[m][n+1] == 0 or n == columns - 1:
+                    perimeter += 1
+    return perimeter
