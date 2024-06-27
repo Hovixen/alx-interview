7c21f2c2fab170646c53ae60b89c08bd85ca5c72
Hovixen <orevaerijo@gmail.com>
2024-06-27 14:36:59 +0000

    task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,12 +17,12 @@ def island_perimeter(grid):
                 if m == 0 or grid[m-1][n] == 0:
                     perimeter += 1
                 # check if down is water [0]
-                if m == rows -1 or grid[m+1][n] == 0:
+                if m == rows - 1 or grid[m+1][n] == 0:
                     perimeter += 1
                 # check if left is water [0]
                 if n == 0 or grid[m][n-1] == 0:
                     perimeter += 1
                 # check if right is water [0]
-                if n == columns -1 or grid[m][n+1] == 0:
+                if n == columns - 1 or grid[m][n+1] == 0:
                     perimeter += 1
     return perimeter
